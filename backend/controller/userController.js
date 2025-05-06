8f2acc4f9f8dc26b310d628a0eb2cb4ddd3a7e17
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-05-06 04:23:21 -0700

    solve the index emi=1 problem

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,13 +1,15 @@
 import user from "../models/userModel.js";
+import brypt, { hash } from 'bcryptjs'
 
 export const singup = async (req, res) => {
    const {firstName , lastName , email,password}=req.body
+   const hashPassowrd = await brypt.hash(password,10)
    try {
     const userExist = await user.findOne({email:email})
     if(userExist){
         return res.status(400).json({message:"user alerady exist"})
     }
-    const newUser = new user({firstName , lastName , email,password})
+    const newUser = new user({firstName , lastName , email,password:hashPassowrd})
     await newUser.save()
     res.status(201).json(newUser)
    } catch (error) {
